0c2e7d1852d3ef305d990e7b035ec57614f241af
Борисов Илья <ilya.borisov@technopark.ru>
2025-02-17 22:13:48 +0300

    Init T-Studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,48 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
-// https://astro.build/config
-export default defineConfig({});
+import relativeLinks from 'astro-relative-links';
+
+import vue from '@astrojs/vue';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  site: 'https://warriorcow.github.io/t-studio/',
+  outDir: 'dist',
+  base: '/t-studio/',
+  trailingSlash: "always",
+  assetsPrefix: 'https://cdn.example.com',
+  compressHTML: false,
+  build: {
+    inlineStylesheets: `never`,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined, // Отключаем разбиение на чанки
+        },
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/styles/vars" as *;
+            @use "@/styles/fonts" as *;
+          `
+        }
+      }
+    }
+  },
+
+  integrations: [relativeLinks(), vue()]
+});
